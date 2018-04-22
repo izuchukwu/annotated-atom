2f1ad6cf4d4ce7259f265280f1051343b8bf7fb0
Izuchukwu Elechi <izuchukwuelechi@gmail.com>
2018-04-22 15:19:09 -0400

    Cleans things up

--- spec/press-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import PressAtomView from '../lib/press-atom-view';
-
-describe('PressAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
